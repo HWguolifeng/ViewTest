609e3fb961debf42fa814f832912e2019526775b
guolifeng <glf_work@163.com>
2017-03-19 16:06:17 +0800

    1：自定义view

---
+++ app/src/main/java/com/guolifeng/android/viewtest/CustomTitleView.java
@@ -0,0 +1,117 @@
+package com.guolifeng.android.viewtest;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+
+/**
+ * Created by 郭利锋 on 2017/3/14 0014.
+ * [简要描述]<BR>
+ */
+
+public class CustomTitleView extends View
+{
+    /**
+     * 文本
+     */
+    private String mTitleText;
+    /**
+     * 文本的颜色
+     */
+    private int mTitleTextColor;
+    /**
+     * 文本的大小
+     */
+    private int mTitleTextSize;
+
+    /**
+     * 绘制时控制文本绘制的范围
+     */
+    private Rect mBound;
+    private Paint mPaint;
+
+    public CustomTitleView(Context context, AttributeSet attrs)
+    {
+        this(context, attrs, 0);
+    }
+
+    public CustomTitleView(Context context)
+    {
+        this(context, null);
+    }
+
+    /**
+     * 获得我自定义的样式属性
+     *
+     * @param context  上下文
+     * @param attrs    属性
+     * @param defStyle 风格
+     */
+    public CustomTitleView(Context context, AttributeSet attrs, int defStyle)
+    {
+        super(context, attrs, defStyle);
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CustomTitleView, defStyle, 0);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++)
+        {
+            int attr = a.getIndex(i);
+            switch (attr)
+            {
+                case R.styleable.CustomTitleView_titleText:
+                    mTitleText = a.getString(attr);
+                    break;
+                case R.styleable.CustomTitleView_titleTextColor:
+                    // 默认颜色设置为黑色
+                    mTitleTextColor = a.getColor(attr, Color.BLACK);
+                    break;
+                case R.styleable.CustomTitleView_titleTextSize:
+                    // 默认设置为16sp，TypeValue也可以把sp转化为px
+                    mTitleTextSize = a.getDimensionPixelSize(attr, (int) TypedValue.applyDimension(
+                            TypedValue.COMPLEX_UNIT_SP, 16, getResources().getDisplayMetrics()));
+                    break;
+
+            }
+
+        }
+        a.recycle();
+
+        /**
+         * 获得绘制文本的宽和高
+         */
+        mPaint = new Paint();
+        mPaint.setTextSize(mTitleTextSize);
+        // mPaint.setColor(mTitleTextColor);
+        mBound = new Rect();
+        mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(200, 200);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        mPaint.setColor(Color.YELLOW);
+        canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint);
+
+        mPaint.setColor(mTitleTextColor);
+        canvas.drawText(mTitleText, getWidth() / 2 - mBound.width() / 2, getHeight() / 2 + mBound.height() / 2, mPaint);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom)
+    {
+        super.onLayout(changed, left, top, right, bottom);
+    }
+}
